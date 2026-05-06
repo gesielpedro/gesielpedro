f9555d707e8dd821be42daefc70ed97b4394efbb
LINX-INVES\gesiel.palbuquerque <gesiel.palbuquerque@linx.com.br>
2026-05-06 17:31:32 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let posts = [];
+
+// GET - listar posts
+app.get('/posts', (req, res) => {
+  res.json(posts);
+});
+
+// POST - criar post
+app.post('/posts', (req, res) => {
+  const { title, content } = req.body;
+
+  const novoPost = {
+    id: Date.now(),
+    title,
+    content
+  };
+
+  posts.push(novoPost);
+  res.status(201).json(novoPost);
+});
+
+app.listen(3000, () => {
+  console.log('API rodando em http://localhost:3000');
+});
